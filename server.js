59f899abe512c667fd7a4fa566e62957c6b08ae0
Zakir <zakirsust2017@gmail.com>
2023-02-14 00:57:08 -0500

    Root route

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ connection.once('open', () => {
     console.log("Connection established")
 })
 
-app.use('/',(req,res)=>{
-    res.send("You are at root directory");
-})
 const studentRouter = require('./api/Student.js');
 app.use('/student', studentRouter);
 
@@ -80,6 +77,9 @@ app.use('/studentlist', studentlist);
 const course = require('./api/Course');
 app.use('/course', course);
 
+app.use('/',(req,res)=>{
+    res.send("You are at root directory");
+})
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
 })
